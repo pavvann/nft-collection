a57aef1625df45f83861e5502661198653e0b199
Pawan <pawanbajaj2012@gmail.com>
2023-02-07 09:02:03 +0530

    changes in api

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -4,8 +4,8 @@ export default function handler(req,res) {
     const tokenId = req.query.tokenId;
     const image_url = "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
     res.status(200).json({
-        name: "Crypto Dev #" + tokenId,
-        description: "Crypto Dev is a collection for developers in crypto",
+        name: "testing #" + tokenId,
+        description: "testing nfts",
         image: image_url + tokenId + ".svg"
     });
 }
